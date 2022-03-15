16312529a1421c967e840d449842d0a88af45c24
BENIASBONONGWE <bed-com-28-19@unima.ac.mw>
2022-03-15 08:14:01 +0200

    my initial commit with partial working class

---
+++ src/btEventHandlers.java
@@ -0,0 +1,20 @@
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author STAR LIGHT COMP TECH
+ */
+public class btEventHandlers implements  EventHandler<ActionEvent>{
+     public void handle(ActionEvent e) {
+ System.out.println("OK button clicked");
+     }
+    
+}
